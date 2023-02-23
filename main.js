c95243c6e33ba5c801e6fea966d5eeb4cc651b94
soumendas12 <102890752+soumendas12@users.noreply.github.com>
2023-02-23 19:32:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+camera = document.getElementById("camera");
